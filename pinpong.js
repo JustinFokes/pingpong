990bbd78908b62471444fb9da0d02069a3f0a16e
Justin Fokes <j.k.fokes@gmail.com>
2016-03-02 14:59:26 -0800

    add javascript to try and solve the problem of dividing input by three and returning ping.

--- pinpong.js
+++ pinpong.js
@@ -1,13 +1,13 @@
-function ping(){
+function pingFunction(){
   var input = ($("input#one").val());
-  if (input === "test"){
-    document.write("test");
+  if (input %3){
+    document.write("ping");
   };
 };
 
 $(document).ready(function(){
   $("form#input-form").submit(function(event){
-ping();
+  pingFunction();
   event.preventDefault();
   });
 });
