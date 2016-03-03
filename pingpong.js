d0ce55bb1fb9a6d48fc86eb1be599bd1ea9427c7
Justin Fokes <j.k.fokes@gmail.com>
2016-03-03 12:04:06 -0800

    javascript taking input, now working to divide input by 3 and return ping

---
+++ pingpong.js
@@ -0,0 +1,17 @@
+function divisibleByThree(inputOne){
+  if (inputOne %3){
+    return ("ping");
+  }
+}
+
+
+$(document).ready(function(){
+  $("form#input-form").submit(function(event){
+    var inputOne = parseInt($("input#one").val());
+
+    var pingPongOne = divisibleByThree(inputOne)
+    $("#result").text(inputOne);
+
+    event.preventDefault();
+  });
+});
