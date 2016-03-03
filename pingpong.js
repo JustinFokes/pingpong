efc3b0a06db926f41e050dfbfcb2f2bcea9fc0b9
Justin Fokes <j.k.fokes@gmail.com>
2016-03-03 12:51:59 -0800

    reversed the code order placing divisibility by 15 first

--- pingpong.js
+++ pingpong.js
@@ -1,14 +1,13 @@
 function divisibleByThree(inputOne){
   //need to write (and not divisible by 15, or, divisible by 3 only...)
-  if (inputOne %3 === 0){
+  if(inputOne %15 === 0){
+    return ("pingpong");
+  }
+  else if (inputOne %3 === 0 && inputOne %15 !== 0){
     return ("ping");
   }
   else if (inputOne %5 === 0){
     return ("pong");
-  }
-  if (inputOne /15){
-    return ("pingpong");
-    //the problem is that anything divisible by 15 is divisible by 3, for instance 15... divisible by both itself and 3. It's running 3 before 15 and hence, is returning "ping" and not "pingpong".
   }
 };
 
