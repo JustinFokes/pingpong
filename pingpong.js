713e93126b64ec436fd28c59be6cec143d09ca83
Justin Fokes <j.k.fokes@gmail.com>
2016-03-04 13:36:44 -0800

    add paragraph elements to the pingDing variable in order to space out input in the results div.

--- pingpong.js
+++ pingpong.js
@@ -1,21 +1,3 @@
-//function divisibleBy(inputOne){
-  //var inputOne = parseInt($("input#one").val());
-  //for (var i = 1; i <=inputOne; i++){
-  //if(inputOne %15 === 0){
-    //return ("pingpong");
-  //}
-  //else if (inputOne %3 === 0 && inputOne %15 !== 0){
-    //return ("ping");
-  //}
-  //else if (inputOne %5 === 0){
-    //return ("pong");
-  //}
-  //else{
-    //return (inputOne)
-    //};
-  //};
-//};
-
 function pingPongOne(){
     var userInput = ($("input#one").val());
     var pingDing = '';
@@ -32,16 +14,20 @@ function pingPongOne(){
     else{
       pingDing = i;
     }
-    $("#result").append(pingDing);
-    //createList.innerHTML = createList.innerHTML + '<p>' + word + '</p>';
+    
+    $("#result").append('<p>' + pingDing + '</p>');
+    
   };
-};
+
+
+}
 
 
 $(document).ready(function(){
   $("form#input-form").submit(function(event){
     
     pingPongOne();
+  
 
     
   
@@ -49,16 +35,5 @@ $(document).ready(function(){
 
     });
   });
-
- //var inputOne = parseInt($("input#one").val());
-  
-    //var pingPongOne = divisibleBy(inputOne);
-
-
-
-  //need to write a loop to take from userInput a specified number range...
-  //or... potentially take users input, create an array, then loop through that array printing each number and replacing certain numbers with ping, pong, and pingpong...
-  
-  //for(var index = 0; index < inputOne; index ++)
 
 
