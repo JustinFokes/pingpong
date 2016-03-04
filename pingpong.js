a3a28f0487a127dc68bb570af8caec7276ea4883
Justin Fokes <j.k.fokes@gmail.com>
2016-03-04 13:41:45 -0800

    will tidy up javascript code by making proper indentations

--- pingpong.js
+++ pingpong.js
@@ -3,36 +3,25 @@ function pingPongOne(){
     var pingDing = '';
     for (var i = 1; i <= userInput; i++){
       if (i %15 === 0){
-    pingDing = 'pingpong';
-    }
-    else if (i %3 === 0 && userInput %15 !== 0){
-    pingDing = 'ping';
-    }
-    else if (i %5 === 0){
-    pingDing = 'pong';
-    }
-    else{
-      pingDing = i;
-    }
-    
+        pingDing = 'pingpong';
+        }
+      else if (i %3 === 0 && userInput %15 !== 0){
+        pingDing = 'ping';
+        }
+      else if (i %5 === 0){
+        pingDing = 'pong';
+        }
+      else{
+        pingDing = i;
+        }
     $("#result").append('<p>' + pingDing + '</p>');
-    
   };
-
-
 }
-
 
 $(document).ready(function(){
   $("form#input-form").submit(function(event){
-    
     pingPongOne();
-  
-
-    
-  
     event.preventDefault();
-
     });
   });
 
