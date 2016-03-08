17eb8ff644a9d87359c8e792d9c331d00a40b420
j <j.k.fokes@gmail.com>
2016-03-07 18:43:03 -0800

    add empty()method

--- pingpong.js
+++ pingpong.js
@@ -1,5 +1,5 @@
 function pingPongOne(){
-    var userInput = ($("input#one").val());
+    var userInput = parseInt($("input#one").val());
     var pingDing = '';
     for (var i = 1; i <= userInput; i++){
       if (i %15 === 0){
@@ -15,14 +15,16 @@ function pingPongOne(){
         pingDing = i;
         }
     $("#result").append('<li>' + pingDing + '</li>');
+
   };
 }
 
 $(document).ready(function(){
   $("form#input-form").submit(function(event){
+  $("#result").empty();
     pingPongOne();
+
+
     event.preventDefault();
     });
   });
-
-
