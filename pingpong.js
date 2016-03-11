4ba9a16e4a36cabba7f48169a4277987567201bf
Justin Fokes <j.k.fokes@gmail.com>
2016-03-10 20:24:15 -0800

    make 15 return pingpong

--- pingpong.js
+++ pingpong.js
@@ -1,28 +1,32 @@
-function pingPongOne(){
-    var userInput = ($("input#one").val());
-    var pingDing = '';
-    for (var i = 1; i <= userInput; i++){
+function pingPongOne(number){
+    var inputAll = [];
+    for (var i = 1; i <= number; i++){
       if (i %15 === 0){
-        pingDing = 'pingpong';
+         inputAll.push("<br>" + "<li>" + 'pingpong' + "</li>");
         }
-      else if (i %3 === 0 && userInput %15 !== 0){
-        pingDing = 'ping';
+      else if (i %3 === 0){
+        inputAll.push("<br>" + "<li>" + 'ping' + "</li>");
         }
       else if (i %5 === 0){
-        pingDing = 'pong';
+        inputAll.push("<br>" + "<li>" + 'pong' + "</li>");
         }
       else{
-        pingDing = i;
-        }
-    $("#result").append('<li>' + pingDing + '</li>');
-  };
+        inputAll.push("<br>" + "<li>" + i + "</li>");
+    }
+  }
+    return inputAll;
 }
 
 $(document).ready(function(){
   $("form#input-form").submit(function(event){
-    pingPongOne();
+    $("#result").empty();
+
+    var numberInput = parseInt($("input#one").val());
+    var pingDing = pingPongOne(numberInput);
+
+    $("#result").html(pingDing);
+
     event.preventDefault();
-    });
+
   });
-
-
+});
